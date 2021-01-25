2045947c6e6644fd7192abfee2407ac4cb56e5c4
Jack Slingsby <jack@theslingsbys.com>
2021-01-25 20:22:16 +0000

    Move sample graph to function

--- Pegasus/Pegasus.py
+++ Pegasus/Pegasus.py
@@ -142,6 +142,15 @@ def PlotLoss(loss_array):
 
 
 
+# %%
+def PlotModelRandomSamle(model):
+    rand_latent = model.getRandomSample()
+    plt.rcParams['figure.dpi'] = 175
+    plt.grid(False)
+    plt.imshow(torchvision.utils.make_grid(rand_latent).cpu().data.permute(0,2,1).contiguous().permute(2,1,0), cmap=plt.cm.binary)
+    plt.show()
+
+
 # %% [markdown] id="Qnjh12UbNFpV"
 # **Define a simple convolutional autoencoder**
 
@@ -193,13 +202,19 @@ class Autoencoder(nn.Module):
 
         self.optimiser = torch.optim.Adam(self.parameters(), lr=1e-4)
     
+    def getRandomSample(self):
+        k = torch.rand(batch_size, latent_size, 1, 1)
+        k = k.to(device)
+        return self.decode(k)
+
+
     def backpropagate(self, loss):
         self.optimiser.zero_grad()
         loss.backward()
         self.optimiser.step()
 
     def trainingStep(self, x, t):
-        # do the forward pass with mean squared error
+        # MSE
         z = self.encode(x)
         x_hat = self.decode(z)
         loss = ((x-x_hat)**2).mean()
@@ -298,6 +313,11 @@ class VAE(nn.Module):
         kl = kl.sum(-1)
         return kl
 
+    def getRandomSample(self):
+        k = torch.rand(batch_size, latent_size)
+        k = k.to(device)
+        return self.decode(k)
+
     def gaussian_likelihood(self, x_hat, logscale, x):
         scale = torch.exp(logscale)
         mean = x_hat
@@ -339,36 +359,11 @@ class VAE(nn.Module):
         return elbo_loss
 
 
-print(f'> Number of autoencoder parameters {len(torch.nn.utils.parameters_to_vector(VAE().parameters()))}')
-'''
-from tqdm import trange
+print(f'> Number of VAE parameters {len(torch.nn.utils.parameters_to_vector(VAE().parameters()))}')
 
-start_epoch = 0
-total_epoch = 1
 
-epoch_iter = trange(start_epoch, total_epoch)
-for epoch in epoch_iter:
-    
-    # array(s) for the performance measures
-    loss_arr = np.zeros(0)
-
-    # iterate over some of the train dateset
-    for i in range(100):
-
-        # sample x from the dataset
-        x,t = next(train_iterator)
-        x,t = x.to(device), t.to(device)
-
-        loss = V.trainingStep(x, t)
-
-        # collect stats
-        loss_arr = np.append(loss_arr, loss.item())
-
-    
-    # print loss
-    epoch_iter.set_description("Current Loss %.5f    Epoch" % loss.item())
-'''
-
+# %% [markdown]
+# ** VAE **
 
 # %%
 V = VAE().to(device)
@@ -376,72 +371,18 @@ elo_loss = TrainModel(V, 5)
 PlotLoss(elo_loss)
 
 # %%
-# sample your model (autoencoders are not good at this)
-k = torch.rand(batch_size, latent_size)
-k = k.to(device)
-g = V.decode(k)
-
-# now show your best batch of data for the submission, right click and save the image for your report
-plt.rcParams['figure.dpi'] = 175
-plt.grid(False)
-plt.imshow(torchvision.utils.make_grid(g).cpu().data.permute(0,2,1).contiguous().permute(2,1,0), cmap=plt.cm.binary)
-plt.show()
+PlotModelRandomSamle(V)
 
 # %% [markdown] id="N1UBl0PJjY-f"
-# **Main training loop**
+# **Auto Encoder**
 
-# %%
+# %% tags=[]
 A = Autoencoder().to(device)
-elo_loss = TrainModel(A, 100)
+elo_loss = TrainModel(A, 5)
 PlotLoss(elo_loss)
 
-# %% colab={"base_uri": "https://localhost:8080/", "height": 1000} id="kb5909Y8D_zx" outputId="eab10264-19a5-43a5-885b-e2580535af74"
-from tqdm import trange
-
-# training loop, you will want to train for more than 10 here!
-start_epoch = 0
-total_epoch = 3
-
-epoch_iter = trange(start_epoch, total_epoch)
-for epoch in epoch_iter:
-    
-    # array(s) for the performance measures
-    loss_arr = np.zeros(0)
-
-    # iterate over some of the train dateset
-    for i in range(100):
-
-        # sample x from the dataset
-        x,t = next(train_iterator)
-        x,t = x.to(device), t.to(device)
-
-        # do the forward pass with mean squared error
-        z = A.encode(x)
-        x_hat = A.decode(z)
-        loss = ((x-x_hat)**2).mean()
-
-        # backpropagate to compute the gradient of the loss w.r.t the parameters and optimise
-        optimiser.zero_grad()
-        loss.backward()
-        optimiser.step()
-
-        # collect stats
-        loss_arr = np.append(loss_arr, loss.item())
-
-    
-    # print loss
-    epoch_iter.set_description("Current Loss %.5f    Epoch" % loss.item())
-
-# %% colab={"base_uri": "https://localhost:8080/", "height": 630} id="liuFFKKE1pZp" outputId="d86ea253-f2e3-4c71-d42b-770d60c7ba51"
-# sample your model (autoencoders are not good at this)
-z = torch.randn_like(z)
-g = A.decode(z)
-
-# now show your best batch of data for the submission, right click and save the image for your report
-plt.rcParams['figure.dpi'] = 175
-plt.grid(False)
-plt.imshow(torchvision.utils.make_grid(g).cpu().data.permute(0,2,1).contiguous().permute(2,1,0), cmap=plt.cm.binary)
-plt.show()
+# %%
+PlotModelRandomSamle(A)
 
 # %%
 # Plot Latent Space
@@ -485,13 +426,11 @@ umap.plot.points(mapper, labels=df["labels"])
 
 # %% id="HjHPwDAe-YoI"
 # optional example code to save your training progress for resuming later if you authenticated Google Drive previously
-torch.save({'A':A.state_dict(), 'optimiser':optimiser.state_dict(), 'epoch':epoch}, './checkpoint.chkpt')
+#torch.save({'A':A.state_dict(), 'optimiser':optimiser.state_dict(), 'epoch':epoch}, './checkpoint.chkpt')
 
 # %% id="nrCN7YQ5-2J8"
 # optional example to resume training if you authenticated Google Drive previously
-params = torch.load('./checkpoint.chkpt')
-A.load_state_dict(params['A'])
-optimiser.load_state_dict(params['optimiser'])
-epoch = params['epoch']
-
-# %%
+#params = torch.load('./checkpoint.chkpt')
+#A.load_state_dict(params['A'])
+#optimiser.load_state_dict(params['optimiser'])
+#epoch = params['epoch']
