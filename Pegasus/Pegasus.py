943c20efa92cd66332d816e30e2b01e969c19203
Jack Slingsby <jack@theslingsbys.com>
2021-01-28 15:55:37 +0000

    Tidy Latent Space playground

--- Pegasus/Pegasus.py
+++ Pegasus/Pegasus.py
@@ -604,42 +604,42 @@ PlotSmallRandomSample(Vres2)
 # %%
 from ipywidgets import interact, interactive, fixed, interact_manual
 import ipywidgets as widgets
+from IPython.display import display
 
-
-# %%
-def on_button_clicked(b):
-    slider_vals = [s.value for s in sliders]
-    #for i in range(16):
-     #   slider_val.append(sliders[i].value)
-
-    with output:
-        output.clear_output()
-        ts = torch.zeros(1,latent_size)
-        ts[0] = torch.FloatTensor(slider_vals)
-        ts = ts.to(device)
-        genImg = Vres2.decode(ts)
-        plotTensor(genImg)
-        #plt.plot(slider_val)
-        #plt.show()
+def LatentSpacePlayground(model):
+    def on_slider_change(b):
+        slider_vals = [s.value for s in sliders]
         
-sliders = []
-row_size = 16
-vb = []
-for i in range(int(math.ceil(latent_size/row_size))):
-    left = latent_size - row_size*i
-    hb = []
-    for j in range(min(row_size, left)):
-        v = i * row_size + j
-        slider = widgets.FloatSlider(description='LV %d'%v, continuous_update=False, orientation='vertical', min=-2, max=2)
-        sliders.append(slider)
-        sliders[-1].observe(on_button_clicked, names='value')
-        hb.append(slider)
-    
-    vb.append(widgets.HBox(hb))
+        with output:
+            output.clear_output()
+            ts = torch.zeros(1,latent_size)
+            ts[0] = torch.FloatTensor(slider_vals)
+            ts = ts.to(device)
+            genImg = model.decode(ts)
+            plotTensor(genImg)
+
+            
+    sliders = []
+    row_size = 16
+    vb = []
+    for i in range(int(math.ceil(latent_size/row_size))):
+        left = latent_size - row_size*i
+        hb = []
+        for j in range(min(row_size, left)):
+            v = i * row_size + j
+            slider = widgets.FloatSlider(description='LV %d'%v, continuous_update=False, orientation='vertical', min=-2, max=2)
+            sliders.append(slider)
+            sliders[-1].observe(on_slider_change, names='value')
+            hb.append(slider)
+        
+        vb.append(widgets.HBox(hb))
 
 
-slider_bank = widgets.VBox(vb)
-output = widgets.Output()
+    slider_bank = widgets.VBox(vb)
+    output = widgets.Output()
 
-#display(slider_bank, output)
+    return (slider_bank, output)
+
+
+#display(*LatentSpacePlayground(Vres))
 
