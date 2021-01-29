4fa23b90a4f3df11a9443d81eb92a69162589755
Jack Slingsby <jack@theslingsbys.com>
2021-01-29 16:58:52 +0000

    Pree-big tidy

--- Pegasus/Pegasus.py
+++ Pegasus/Pegasus.py
@@ -215,6 +215,16 @@ def PlotModelSampleEncoding(model):
     plt.imshow(torchvision.utils.make_grid(x[0:10]).cpu().data.permute(0,2,1).contiguous().permute(2,1,0), cmap=plt.cm.binary)
     plt.show()
 
+def PlotCompareModels(model1, model2):
+    x,t = next(train_iterator)
+    x = x[0:8]
+    x = x.to(device)
+    _, _, _, x_hat_a = model1.trainingStep(x, t)
+    _, _, _, x_hat_b = model2.trainingStep(x, t)
+    plotTensor(x)
+    plotTensor(x_hat_a)
+    plotTensor(x_hat_b)
+
 
 # %%
 def PlotSmallRandomSample(model, count=8):
@@ -548,12 +558,12 @@ vae_dec = resnet18_decoder(
     input_height=image_size
 )
 Vres = VAE(vae_enc, vae_dec).to(device)
-elo_loss, kl_loss, recon_loss = TrainModel(Vres, 10)
+elo_loss, kl_loss, recon_loss = TrainModel(Vres, 1300)
 PlotAllLoss([elo_loss, kl_loss, recon_loss], ["EBLO", "KL", "Recon"])
 PlotLoss(elo_loss)
 
 # %%
-CheckpointModel(Vres, 'VresClean18-10hr')
+CheckpointModel(Vres, 'VresClean18-10hr', 1300)
 
 # %%
 PlotModelRandomGeneratedSample(Vres)
@@ -621,3 +631,28 @@ vae_old_dec = resnet18_decoder(
 )
 Vres_old = VAE(vae_old_enc, vae_old_dec).to(device)
 RestoreModel(Vres_old, 'Vres-20hr')
+
+# %%
+PlotModelSampleEncoding(Vres_old)
+
+# %%
+elo_loss, kl_loss, recon_loss = TrainModel(Vres_old, 1)
+
+# %%
+PlotCompareModels(Vres_old, Vres)
+
+# %%
+from ResNetExample import resnet18_encoder, resnet18_decoder
+vae_test_enc = resnet18_encoder(True, True)
+vae_test_dec = resnet18_decoder(
+    latent_dim=latent_size,
+    input_height=image_size,
+    first_conv=False,
+    maxpool1=False
+)
+Vtest = VAE(vae_test_enc, vae_test_dec).to(device)
+elo_loss2, kl_loss2, recon_loss2 = TrainModel(Vtest, 200)
+PlotAllLoss([elo_loss2, kl_loss2, recon_loss2], ["EBLO", "KL", "Recon"])
+PlotLoss(elo_loss)
+
+# %%
