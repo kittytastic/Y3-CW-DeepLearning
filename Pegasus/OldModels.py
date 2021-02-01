3c89bb38b2fb30cedbeaac2c835ea952c5e3b663
Jack Slingsby <jack@theslingsbys.com>
2021-02-01 00:16:35 +0000

    Finish tidy resnet

--- Pegasus/OldModels.py
+++ Pegasus/OldModels.py
@@ -272,3 +272,143 @@ vae_old_dec = resnet18_decoder(
 Vres_old = VAE(vae_old_enc, vae_old_dec).to(device)
 RestoreModel(Vres_old, 'Vres-20hr')
 '''
+
+from ResNet import EncoderBlock, DecoderBlocker
+class ResNetEncoder(nn.Module):
+
+    def __init__(self, layers):
+        super().__init__()
+
+        self.current_planes = 64 # As per paper - we start with 64 planes
+
+        # Layer 1
+        self.conv1 = nn.Conv2d(3, self.current_planes, kernel_size=7, stride=2, padding=3, bias=True)
+        self.bn1 = nn.BatchNorm2d(self.current_planes)
+        self.relu = nn.ReLU(inplace=True)
+
+        # Layer 2
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.conv_layer2 = self._make_layer(64, layers[0], stride=1)
+
+        # Layer 3 -> 5
+        self.conv_layer3 = self._make_layer(128, layers[1], stride=2)
+        self.conv_layer4 = self._make_layer(256, layers[2], stride=2)
+        self.conv_layer5 = self._make_layer(512, layers[3], stride=2)
+        
+        # Flatten planes into 1 number HxW
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+
+    def _make_layer(self, layer_planes, blocks, stride=None):
+        downsample = None
+
+        # Each layer spatially downsamples the previous layer's output
+        # We don't want to do this on the first layer as that is done in a special conv
+        if stride != 1:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.current_planes, layer_planes, kernel_size=1, stride=stride, bias=True),
+                nn.BatchNorm2d(layer_planes),
+            )
+
+
+        layers = []
+
+        # If we downsample then the first of n block downsamples
+        layers.append(EncoderBlock(self.current_planes, layer_planes, stride, downsample))
+        self.current_planes = layer_planes
+
+        # The rest don't touch the dimensions
+        for _ in range(1, blocks):
+            layers.append(EncoderBlock(layer_planes, layer_planes))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = nn.Sequential(
+            self.conv1,
+            self.bn1,
+            self.relu,
+            self.maxpool,
+            self.conv_layer2,
+            self.conv_layer3,
+            self.conv_layer4,
+            self.conv_layer5,
+            self.avgpool,
+        )(x)
+        
+        x = torch.flatten(x, 1)
+        return x
+
+
+class ResNetDecoder(nn.Module):
+    """
+    Resnet in reverse order
+    """
+
+    def __init__(self, layers, latent_dim, input_height):
+        super().__init__()
+
+        self.current_planes = 512 # As per paper
+        self.input_height = input_height
+
+        # Linear layer to take us from latent to 4 x 4 x planes 
+        self.linear = nn.Linear(latent_dim, self.current_planes * 4 * 4)
+        
+        # interpolate after linear layer using scale factor
+        self.upscale1 = nn.Upsample(size=input_height // 32)
+
+        # Opposite Layer 5 -> 3
+        self.conv_layer5 = self._make_layer(256, layers[0], scale=2)
+        self.conv_layer4 = self._make_layer(128, layers[1], scale=2)
+        self.conv_layer3 = self._make_layer(64, layers[2], scale=2)
+
+        # Reverse second layer 3x3 stride 2
+        self.conv_layer2 = self._make_layer(64, layers[3], scale=2)
+        
+        # Reverse 7x7 stride 2 downsampling
+        self.upscale = nn.Upsample(scale_factor=2)
+        self.conv1 = nn.Conv2d(64, 3, kernel_size=3, stride=1, padding=1, bias=True)
+
+        self.renorm = nn.Sigmoid()
+
+    def _make_layer(self, target_planes, blocks, scale=1):
+        upsample = None
+        if scale != 1:
+            upsample = nn.Sequential(
+                nn.Upsample(scale_factor=scale),
+                nn.Conv2d(self.current_planes, target_planes , kernel_size=1, stride=1),
+                nn.BatchNorm2d(target_planes),
+            )
+
+        layers = []
+        layers.append(DecoderBlock(self.current_planes, target_planes, scale, upsample))
+        self.current_planes = target_planes
+
+        for _ in range(1, blocks):
+            layers.append(DecoderBlock(self.current_planes, target_planes))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.linear(x)
+        x = x.view(x.size(0), 512, 4, 4)
+
+        x = nn.Sequential(
+            self.upscale1,
+            self.conv_layer5,
+            self.conv_layer4,
+            self.conv_layer3,
+            self.conv_layer2,
+            self.upscale,
+            self.conv1,
+            self.renorm,
+        )(x)
+        return x
+
+
+def ResNet18Encoder():
+    return ResNetEncoder([2, 2, 2, 2])
+
+
+def ResNet18Decoder(latent_dim, input_height):
+    return ResNetDecoder([2, 2, 2, 2], latent_dim, input_height)
+
--- Pegasus/ResNet.py
+++ Pegasus/ResNet.py
@@ -1,15 +1,18 @@
 
 #
-# this code is based on https://github.com/PyTorchLightning/pytorch-lightning-bolts/pl_bolts/models/autoencoders/components.py, which is released under the Apache 2.0 licesne
-# https://github.com/pytorch/vision/blob/master/torchvision/models/resnet.py
+# this code is based on https://github.com/PyTorchLightning/pytorch-lightning-bolts/pl_bolts/models/autoencoders/components.py, which is released under the Apache 2.0 License
+# which in turn used portions of code from https://github.com/pytorch/vision/blob/master/torchvision/models/resnet.py which is released under BSD 3-Clause License
 #
+# Resnet paper: https://arxiv.org/pdf/1512.03385.pdf
+#
+
 import torch
 from torch import nn
-from torch.nn import functional as F
+#from torch.nn import functional as F
 
+
+# Simple 2 conv block
 class EncoderBlock(nn.Module):
-    expansion = 1
-
     def __init__(self, in_planes, working_planes, stride=1, downsample=None):
         super().__init__()
         self.conv1 = nn.Conv2d(in_planes, working_planes, kernel_size=3, stride=stride, padding=1, bias=True)
@@ -36,18 +39,17 @@ class EncoderBlock(nn.Module):
             self.bn2,
         )(x)
 
-        out += identity # The skip connection
+        out += identity # The magic skip connection
         out = self.relu(out)
         return out
 
 
+# Simple 2 conv block
 class DecoderBlock(nn.Module):
-    # Mirror image of Encode Block - kind of; skip connection follows dataflow
+    # Encoder block in reverse
     #           Conv1       Conv2
     # Encode    Shrink         -
     # Decode      -         Grow
-
-    expansion = 1
 
     def __init__(self, working_planes, out_planes, scale=1, upsample=None):
         super().__init__()
@@ -77,151 +79,16 @@ class DecoderBlock(nn.Module):
             self.bn2,
         )(x)
 
-        out += identity # The skip connection
+        out += identity # The magic skip connection
         out = self.relu(out)
         return out
 
-
-
-class ResNetEncoder(nn.Module):
-
-    def __init__(self, layers):
-        super().__init__()
-
-        self.current_planes = 64 # As per paper - we start with 64 planes
-
-        # Layer 1
-        self.conv1 = nn.Conv2d(3, self.current_planes, kernel_size=7, stride=2, padding=3, bias=True)
-        self.bn1 = nn.BatchNorm2d(self.current_planes)
-        self.relu = nn.ReLU(inplace=True)
-
-        # Layer 2
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.conv_layer2 = self._make_layer(64, layers[0], stride=1)
-
-        # Layer 3 -> 5
-        self.conv_layer3 = self._make_layer(128, layers[1], stride=2)
-        self.conv_layer4 = self._make_layer(256, layers[2], stride=2)
-        self.conv_layer5 = self._make_layer(512, layers[3], stride=2)
-        
-        # Flatten planes into 1 number HxW
-        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-
-    def _make_layer(self, layer_planes, blocks, stride=None):
-        downsample = None
-
-        # Each layer spatially downsamples the previous layer's output
-        # We don't want to do this on the first layer as that is done in a special conv
-        if stride != 1:
-            downsample = nn.Sequential(
-                nn.Conv2d(self.current_planes, layer_planes, kernel_size=1, stride=stride, bias=True),
-                nn.BatchNorm2d(layer_planes),
-            )
-
-
-        layers = []
-
-        # If we downsample then the first of n block downsamples
-        layers.append(EncoderBlock(self.current_planes, layer_planes, stride, downsample))
-        self.current_planes = layer_planes
-
-        # The rest don't touch the dimensions
-        for _ in range(1, blocks):
-            layers.append(EncoderBlock(layer_planes, layer_planes))
-
-        return nn.Sequential(*layers)
-
-    def forward(self, x):
-        x = nn.Sequential(
-            self.conv1,
-            self.bn1,
-            self.relu,
-            self.maxpool,
-            self.conv_layer2,
-            self.conv_layer3,
-            self.conv_layer4,
-            self.conv_layer5,
-            self.avgpool,
-        )(x)
-        
-        x = torch.flatten(x, 1)
-        return x
-
-
-class ResNetDecoder(nn.Module):
-    """
-    Resnet in reverse order
-    """
-
-    def __init__(self, layers, latent_dim, input_height):
-        super().__init__()
-
-        self.current_planes = 512 # As per paper
-        self.input_height = input_height
-
-        # Linear layer to take us from latent to 4 x 4 x planes 
-        self.linear = nn.Linear(latent_dim, self.current_planes * 4 * 4)
-        
-        # interpolate after linear layer using scale factor
-        self.upscale1 = nn.Upsample(size=input_height // 32)
-
-        # Opposite Layer 5 -> 3
-        self.conv_layer5 = self._make_layer(256, layers[0], scale=2)
-        self.conv_layer4 = self._make_layer(128, layers[1], scale=2)
-        self.conv_layer3 = self._make_layer(64, layers[2], scale=2)
-
-        # Reverse second layer 3x3 stride 2
-        self.conv_layer2 = self._make_layer(64, layers[3], scale=2)
-        
-        # Reverse 7x7 stride 2 downsampling
-        self.upscale = nn.Upsample(scale_factor=2)
-        self.conv1 = nn.Conv2d(64, 3, kernel_size=3, stride=1, padding=1, bias=True)
-
-        self.renorm = nn.Sigmoid()
-
-    def _make_layer(self, target_planes, blocks, scale=1):
-        upsample = None
-        if scale != 1:
-            upsample = nn.Sequential(
-                nn.Upsample(scale_factor=scale),
-                nn.Conv2d(self.current_planes, target_planes , kernel_size=1, stride=1),
-                nn.BatchNorm2d(target_planes),
-            )
-
-        layers = []
-        layers.append(DecoderBlock(self.current_planes, target_planes, scale, upsample))
-        self.current_planes = target_planes
-
-        for _ in range(1, blocks):
-            layers.append(DecoderBlock(self.current_planes, target_planes))
-
-        return nn.Sequential(*layers)
-
-    def forward(self, x):
-        x = self.linear(x)
-        x = x.view(x.size(0), 512, 4, 4)
-
-        x = nn.Sequential(
-            self.upscale1,
-            self.conv_layer5,
-            self.conv_layer4,
-            self.conv_layer3,
-            self.conv_layer2,
-            self.upscale,
-            self.conv1,
-            self.renorm,
-        )(x)
-        return x
-
-
-def ResNet18Encoder():
-    return ResNetEncoder([2, 2, 2, 2])
-
-
-def ResNet18Decoder(latent_dim, input_height):
-    return ResNetDecoder([2, 2, 2, 2], latent_dim, input_height)
-
-
+#
+# Fully connected layer
+# Take "in_dim" input, gives "out_dim" outputs
+# Uses "depth" fully connected layers
+# Intermediate layers are logarithmically scaled
+#
 class FCLayer(nn.Module):
     def __init__(self, in_dim, out_dim, depth):
         super().__init__()
@@ -242,7 +109,9 @@ class FCLayer(nn.Module):
     def forward(self, x):
         return self.layers(x)
 
-class FCCResNetEncoder(nn.Module):
+# Resnet encoder
+# However use a fully connected layer at the end to take us from conv space to latent space
+class FCResNetEncoder(nn.Module):
 
     def __init__(self, layers, latent_size, fc_depth):
         super().__init__()
@@ -311,10 +180,11 @@ class FCCResNetEncoder(nn.Module):
         return x
 
 
-class FCCResNetDecoder(nn.Module):
-    """
-    Resnet in reverse order
-    """
+# Resnet decoder
+# Does what the decoder does but backwards
+# Uses a final 3x3 kernel instead of 7x7 to help recreate finer details
+# Uses a fully connected layer at the begining to take us from latent space to conv space
+class FCResNetDecoder(nn.Module):
 
     def __init__(self, layers, latent_dim, out_img_size, fc_depth):
         super().__init__()
@@ -332,7 +202,7 @@ class FCCResNetDecoder(nn.Module):
         # Reverse second layer 3x3 stride 2
         self.conv_layer2 = self._make_layer(64, layers[3], scale=2) # 32 x 16x x 16x
         
-        # Reverse 7x7 stride 2 downsampling
+        # Reverse 7x7 stride 2 downsampling, although use 3x3 kernel to help with finer details
         self.upscale = nn.Upsample(scale_factor=2)
         self.conv1 = nn.Conv2d(64, 3, kernel_size=3, stride=1, padding=1, bias=True) # 16 x 32x x 32x 
 
@@ -372,11 +242,13 @@ class FCCResNetDecoder(nn.Module):
         return x
 
 
-        
-def FCCResNet18Encoder(latent_size):
-    return FCCResNetEncoder([2, 2, 2, 2], latent_size, 3)
+# Uses the 2,2,2,2 config as proposed in paper
+# There may be better distributions for layers
+# but without an abundance of time I will assume Zhang et.al
+# found 2,2,2,2 to be particually good for the number of parameters    
+def FCResNet18Encoder(latent_size):
+    return FCResNetEncoder([2, 2, 2, 2], latent_size, 3)
 
 
-def FCCResNet18Decoder(latent_size, img_size):
-    return FCCResNetDecoder([2, 2, 2, 2], latent_size, img_size, 3)
-
+def FCResNet18Decoder(latent_size, img_size):
+    return FCResNetDecoder([2, 2, 2, 2], latent_size, img_size, 3)
