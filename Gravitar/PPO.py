81095febf890fc5175dcca2cc82afef687e4e431
Jack Slingsby <jack@theslingsbys.com>
2021-02-04 00:59:39 +0000

    Working PPO agent

--- Gravitar/PPO.py
+++ Gravitar/PPO.py
@@ -20,7 +20,7 @@ def itemgetter(*items):
 
 # Need an optimiser for actor critic
 class ActorCritic(nn.Module):
-    def __init__(self, num_inputs, num_outputs, hidden_size, std=0.0):
+    def __init__(self, num_inputs, num_outputs, hidden_size, std=0.0, lr=3e-4):
         super(ActorCritic, self).__init__()
         
         self.critic = nn.Sequential(
@@ -35,6 +35,8 @@ class ActorCritic(nn.Module):
             nn.Linear(hidden_size, num_outputs),
             nn.Softmax(dim=0),
         )
+
+        self.optimizer = optim.Adam(self.parameters(), lr=lr)
        
 
     def chooseAction(self, state):
@@ -46,8 +48,31 @@ class ActorCritic(nn.Module):
     def getCriticFor(self, state):
         return self.critic(state)
 
-    def learn(self, stuff):
-        pass
+    def learn(self, epochs, experience, clip_epsilon=0.2):
+        
+        
+        for _ in range(epochs):
+            for mini_experience_batch in miniBatchIter(10, experience):
+            
+                state, action, old_log_probs, retruns, advantage = itemgetter('states', 'actions', 'log_probs', 'returns', 'advantage' )(mini_experience_batch)
+                
+                _, dist = self.chooseAction(state)
+                value = self.getCriticFor(state)
+                entropy = dist.entropy().mean()
+                new_log_probs = dist.log_prob(action)
+
+                ratio = (new_log_probs - old_log_probs).exp()
+                surr1 = ratio * advantage
+                surr2 = torch.clamp(ratio, 1.0-clip_epsilon, 1.0+clip_epsilon)*advantage
+
+                actor_loss = -torch.min(surr1, surr2).mean()
+                critic_loss = (retruns - value).pow(2).mean()
+
+                loss = 0.5 * critic_loss + actor_loss - 0.001 * entropy
+
+                self.optimizer.zero_grad()
+                loss.backward()
+                self.optimizer.step()
 
 
 def testReward(env, actor_critic):
@@ -55,10 +80,13 @@ def testReward(env, actor_critic):
     state = env.reset()
     done = False
     while not done:
+        state = torch.FloatTensor(state)
         action, _ = actor_critic.chooseAction(state)
-        next_state, reward, done, _ = env.step(action)
+        next_state, reward, done, _ = env.step(action.cpu().numpy())
         state = next_state
         total_reward += reward
+
+    return total_reward
 
 def accrueExperience(env, actor_critic, steps=128):
 
@@ -68,6 +96,7 @@ def accrueExperience(env, actor_critic, steps=128):
     probs = []
     masks = []
     values = []
+    log_probs = []
 
     state = env.reset()
     for e in range(steps):
@@ -76,7 +105,9 @@ def accrueExperience(env, actor_critic, steps=128):
         estimated_value = actor_critic.getCriticFor(state)
 
         next_state, reward, done, _ = env.step(action.cpu().numpy())
-        
+
+        log_prob = action_distribution.log_prob(action)
+        log_probs.append(log_prob)       
         masks.append(int(not done)) # Used to mask out
         rewards.append(reward)
         states.append(state)
@@ -93,11 +124,11 @@ def accrueExperience(env, actor_critic, steps=128):
     next_state = torch.FloatTensor(next_state)
     next_value = actor_critic.getCriticFor(next_state)
 
-    return {'masks':masks, 'rewards':rewards, 'states':states, 'actions':actions, 'probs':probs, 'values':values}, next_value
+    return {'masks':masks, 'rewards':rewards, 'states':states, 'actions':actions, 'log_probs': log_probs, 'probs':probs, 'values':values}, next_value
 
 def proccessExperiences(next_value, raw_experience, gamma=0.99, tau=0.95):
     masks, rewards, values = itemgetter('masks', 'rewards', 'values' )(raw_experience)
-    values += [next_value]
+    values = values + [next_value]
 
     # Calculate General advantage estimation and returns for each step
     gae = 0
@@ -109,11 +140,11 @@ def proccessExperiences(next_value, raw_experience, gamma=0.99, tau=0.95):
     
     return {**raw_experience, 'returns':retruns}
 
-def miniBatchIter(mini_batch_size, mini_batch_count, experiences):
-    batch_size = len(experiences[experiences.keys()[0]])
-    for _ in range(mini_batch_count):
+def miniBatchIter(mini_batch_size, experiences):
+    batch_size = len(experiences[list(experiences.keys())[0]])
+    for _ in range(batch_size//mini_batch_size):
         rand_ids = np.random.randint(0, batch_size, mini_batch_size)
-        yield {key: field[rand_ids, :] for key, field in experiences}
+        yield {key: field[rand_ids] for key, field in experiences.items()}
 
 
 def teachActorCritic(actor_critic, experiences, epochs=10):
@@ -123,9 +154,15 @@ def teachActorCritic(actor_critic, experiences, epochs=10):
     
 
 
-#env_name = 'Gravitar-ram-v0'
-env_name = 'CartPole-v0'
+
+video_every = 1
+env_name = 'Gravitar-ram-v0'
+#env_name = 'CartPole-v0'
+env_name = 'SpaceInvaders-ram-v0'
 env = gym.make(env_name)
+env_test = gym.make(env_name)
+#env = gym.wrappers.Monitor(env, "./video", video_callable=lambda episode_id: (episode_id%video_every)==0, force=True)
+env_test = gym.wrappers.Monitor(env, "./video", video_callable=lambda episode_id: (episode_id%video_every)==0, force=True)
 
 
 assert(len(env.observation_space.shape)==1)
@@ -136,8 +173,21 @@ print("inputs: %d   outputs: %d   for: %s"%(num_inputs, num_outputs, env_name))
 
 model = ActorCritic(num_inputs, num_outputs, 128)
 
-raw_experience, next_value = accrueExperience(env, model, 2)
+for i in range(1000):
+    raw_experience, next_value = accrueExperience(env, model, 128)
 
-experience = proccessExperiences(next_value, raw_experience)
-#print(experience)
-#print(raw_experience)
+    experience = proccessExperiences(next_value, raw_experience)
+
+    returns   = torch.cat(experience['returns']).detach()
+    log_probs = torch.Tensor(experience['log_probs']).detach()
+    values    = torch.Tensor(experience['values']).detach()
+    states = torch.Tensor()
+    states    = torch.cat(experience['states']).view(len(experience['states']), -1)
+    actions   = torch.Tensor(experience['actions'])
+    advantage = returns - values
+
+    experience = {'returns':returns, 'log_probs':log_probs, 'values':values, 'states':states, 'actions':actions, 'advantage':advantage}
+    model.learn(10, experience)
+
+    if i % 10 == 0:
+        print(testReward(env_test, model))
